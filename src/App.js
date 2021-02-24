0507fe0cc825afb0fb3c740813bd4252c5d722d2
lafBot <64456835+lafBot@users.noreply.github.com>
2021-02-23 23:38:59 -0700

    Completed App functions for adding, editing, and deleting blog posts

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter } from "react-router-dom";
+import Routes from "./Routes";
+import Header from "./Header";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
+  const [blog, setBlog] = useState([
+        { id: "-a1ba-43c8-de80d9e89ccb-c3a476b2ee6f",
+          title: "Hypercode",
+          description: "A beginners guide to coding",
+          body: "To get the best out of coding you need to practice. Practice makes perfect.  Perfect is made by practice.  Keep practicing to get better.  Work on projects.... aka... practice."
+        }]);
+
+  function addPost(newPost) {
+    setBlog([...blog, newPost]);
+  }
+
+  function editPost(updatedPost) {
+    setBlog(blog.map(post => (
+      post.id === updatedPost.id
+        ? updatedPost
+        : post
+    )))
+  }
+
+  function deletePost(idToRemove) {
+    setBlog(blog.filter(post => post.id !== idToRemove))
+  }
+
+return (
+
+  <div className="App">
+    <Header />
+      <BrowserRouter>
+        <div className="App-body">
+          <Routes blog={blog} addPost={addPost} editPost={editPost} deletePost={deletePost} />
+        </div>
+      </BrowserRouter>
+  </div>
+);
 }
 
 export default App;
