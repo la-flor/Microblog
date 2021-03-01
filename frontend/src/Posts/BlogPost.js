2cba07733c220e94882f435a429c25e91a9c896f
lafBot <64456835+lafBot@users.noreply.github.com>
2021-03-01 15:34:37 -0700

    Moved voting function into Home.js and changed button function call

--- frontend/src/Posts/BlogPost.js
+++ frontend/src/Posts/BlogPost.js
@@ -1,20 +1,15 @@
 import React from "react";
 import "./BlogPost.css";
 
-import { changeVote } from "../actions/titles";
-
-const BlogPost = ({ titleId, title, votes, description }) => {
-    function vote(direction) {
-        changeVote(titleId, direction)
-    }
+const BlogPost = ({ titleId, title, votes, description, vote }) => {
 
     return (
         <div id={titleId} className="BlogPost p-5 my-3">
             <h3><a href={titleId}>{title}</a></h3>
             <p><i>{description}</i></p>
             <span className="BlogPost-voteCount mr-3">{votes} votes</span>
-            <span className="BlogPost-thumbsUp mr-3" onClick={e => vote("up")}><i className="fas fa-thumbs-up"></i></span>
-            <span className="BlogPost-thumbsDown" onClick={e => vote("down")}><i className="fas fa-thumbs-down"></i></span>
+            <span className="BlogPost-thumbsUp mr-3" onClick={e => vote(titleId, "up")}><i className="fas fa-thumbs-up"></i></span>
+            <span className="BlogPost-thumbsDown" onClick={e => vote(titleId, "down")}><i className="fas fa-thumbs-down"></i></span>
         </div>
     )
 }
